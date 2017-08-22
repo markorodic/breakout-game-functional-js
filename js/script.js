9602a4c3ccaacbb42644ab16d60e6158f4b9585f
Marko Rodic <mrmarkorodic@googlemail.com>
2017-08-22 23:40:19 +0100

    move paddle left and right

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ window.onload = function() {
 
     function play(state, canvas, ctx) {
         draw({ state: state, canvas, ctx })
+        updatePlayer()
         window.requestAnimationFrame(function(){
             play(state, canvas, ctx)
         })
@@ -35,6 +36,35 @@ window.onload = function() {
         size: { x: 20, y: 7 }
     }
 
+    const keys = {
+        LEFT: { x: -4 },
+        RIGHT: { x: +4 },
+        SPACE: { x: 0, y: 0 }
+    }
+
+    const whichKey = {
+        37: 'LEFT_KEY',
+        39: 'RIGHT_KEY',
+        32: 'SPACE_KEY'
+    }
+
+    let keyState = null
+
+    window.onkeydown = (e) => {
+        keyState = whichKey[e.keyCode]
+    }
+
+    window.onkeyup = (e) => keyState = null
+
+    function updatePlayer() {
+        if (keyState == 'LEFT_KEY') {
+            setupPlayer.position.x += keys.LEFT.x
+        }
+        if (keyState == 'RIGHT_KEY') {
+            setupPlayer.position.x += keys.RIGHT.x
+        }
+    }
+
     function draw({ state = {}, canvas = null, ctx }) {
         [drawBall, drawPlayer, drawBricks].forEach(function(f) {
             f({ state, canvas, ctx })
@@ -47,11 +77,11 @@ window.onload = function() {
 
     function drawBricks({state, canvas, ctx}) {
         var bricks = []
-            for (var i = 0; i < 540; i++) {
-                var x = 22 + (i % 20) * 24
-                var y = 40 + (i % 27) * 10
-                ctx.fillRect(x - state.bricks.size.x / 2, y - state.bricks.size.y / 2, state.bricks.size.x, state.bricks.size.y)
-            }
+        for (var i = 0; i < 540; i++) {
+            var x = 22 + (i % 20) * 24
+            var y = 40 + (i % 27) * 10
+            ctx.fillRect(x - state.bricks.size.x / 2, y - state.bricks.size.y / 2, state.bricks.size.x, state.bricks.size.y)
+        }
     }
 
     function drawPlayer({state, canvas, ctx}) {
